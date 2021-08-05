50f1e12aa5c591dcb4b4388e486781ca90fc8b92
Nguyễn Thị Thúy <thuynguyen03091999@gmail.com>
2021-08-05 17:27:23 +0700

    Add meals

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/categories_screen.dart';
+import 'package:meals_app/category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -26,7 +27,11 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DeliMeals',
-      home: CategoriesScreen(),
+      // home: CategoriesScreen(),
+      routes: {
+        '/': (context) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (context) => CategoryMealsScreen()
+      },
       theme: ThemeData(
           primarySwatch: Colors.pink,
           accentColor: Colors.amber,
